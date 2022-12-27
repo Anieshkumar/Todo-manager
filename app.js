6ba8dbb82f7325a55ab5047c535e4bc29c7e2362
Anieshkumar <Anieshkumar709@gmail.com>
2022-12-27 14:28:44 +0530

    9 lEVEL 1st comm

--- app.js
+++ app.js
@@ -2,52 +2,51 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
+app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.set("view engine","ejs")
-app.get("/", async (request,response) => {
+app.set("view engine", "ejs");
+app.get("/", async (request, response) => {
   const overdue = await Todo.overdue();
   const dueToday = await Todo.dueToday();
   const dueLater = await Todo.dueLater();
   const completedTodo = await Todo.completedTodo();
-  if(request.accepts("html")) {
-    response.render('index', {
+  if (request.accepts("html")) {
+    response.render("index", {
       overdue,
       dueToday,
       dueLater,
-      completedTodo
+      completedTodo,
     });
-  } else{
+  } else {
     response.json({
       overdue,
       dueToday,
       dueLater,
-      completedTodo
-    })
+      completedTodo,
+    });
   }
-  
 });
-app.get("/", async (request,response) => {
+app.get("/", async (request, response) => {
   const overdue = await Todo.overdue();
-  if(request.accepts("html")) {
-    response.render('index', {
-      overdue
+  if (request.accepts("html")) {
+    response.render("index", {
+      overdue,
     });
-  } else{
+  } else {
     response.json({
-      overdue
-    })
+      overdue,
+    });
   }
-  
 });
 
 app.get("/", function (request, response) {
   response.send("Hello World");
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // eslint-disable-next-line no-unused-vars
 app.get("/todos", async function (_request, response) {
@@ -60,7 +59,6 @@ app.get("/todos", async function (_request, response) {
     console.log(error);
     return response.status(422).json(error);
   }
-
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -75,8 +73,11 @@ app.get("/todos/:id", async function (request, response) {
 
 app.post("/todos", async function (request, response) {
   try {
-    const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
@@ -96,20 +97,19 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 // eslint-disable-next-line no-unused-vars
 app.delete("/todos/:id", async function (request, response) {
-    try{
-  console.log("We have to delete a Todo with ID: ", request.params.id);
+  try {
+    console.log("We have to delete a Todo with ID: ", request.params.id);
 
-  const todo = await Todo.destroy({
-    where: {
-      id: request.params.id,
-    },
-  });
-  response.send(todo ? true : false);
-} catch(error) {
-  console.log(error);
-  return response.status(422).json(error);
-}
-
+    const todo = await Todo.destroy({
+      where: {
+        id: request.params.id,
+      },
+    });
+    response.send(todo ? true : false);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 module.exports = app;
--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model, Op
-} = require('sequelize');
+"use strict";
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -12,15 +10,14 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-    static addTodo({ title, dueDate}) {
-      return this.create({ title: title,dueDate: dueDate,completed: false });
-
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-    static getTodo(){
+    static getTodo() {
       return this.findAll();
     }
     markAsCompleted() {
-      return this.update({ completed:true })
+      return this.update({ completed: true });
     }
     static async overdue() {
       return await Todo.findAll({
@@ -51,22 +48,25 @@ module.exports = (sequelize, DataTypes) => {
         order: [["id", "ASC"]],
       });
     }
-  
+
     static async completedTodo() {
       return await Todo.findAll({
         where: {
           completed: true,
-      },
-    });
+        },
+      });
+    }
   }
-}
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
   return Todo;
 };
